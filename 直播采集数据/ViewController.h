6ec08aa033fced6a44fca22cde6c1d8e3f9717f2
txy <txy@txydeimac.local>
2016-06-08 15:58:36 +0800

    Initial Commit

---
+++ 直播采集数据/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  直播采集数据
+//
+//  Created by txy on 16/6/8.
+//  Copyright © 2016年 txy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
